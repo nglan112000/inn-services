12978237ddb9321c19b879ae5e8590f64750c2cf
Lan Nguyen <nglan112000@gmail.com>
2022-11-01 13:07:00 +0700

    add feature finding roommate

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -4,14 +4,14 @@ import userReducer from './userReducer';
 import loginReducer from './loginReducer';
 import innReducer from './innReducer';
 import logisticReducer from './logisticReducer';
-import messageReducer from './messageReducer';
+import roommateReducer from './roommateReducer';
 
 const allReducers = combineReducers({
   userReducer,
   loginReducer,
   innReducer,
   logisticReducer,
-  messageReducer,
+  roommateReducer,
 });
 
 export default allReducers;
